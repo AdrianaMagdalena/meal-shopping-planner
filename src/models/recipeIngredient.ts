ee99473fdd71f3dbe315a417cac93420fb6cc82d
AdrianaMagdalena <128493793+AdrianaMagdalena@users.noreply.github.com>
2026-09-24 22:39:21 +0200

    Remove static html on weekly plan to prepare for classes, update css, remove unused files

--- src/models/recipeIngredient.ts
+++
@@ -1,7 +0,0 @@
-export class RecipeIngredient {
-  constructor(
-    public recipeId: string,
-    public quantity: number,
-    public optional?: boolean,
-  ) {}
-}
--- src/models/recipePart.ts
+++
@@ -1,9 +0,0 @@
-import { RecipeIngredient } from "./recipeIngredient.js";
-
-export class RecipePart {
-  constructor(
-    public ingredients: RecipeIngredient[],
-    public steps: string[],
-    public partTitle?: string,
-  ) {}
-}
